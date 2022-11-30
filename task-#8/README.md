5b999dd0439a487c0be92e98b15e091601aceb43
Mindless-Magnet <muhammedshajahan45@gmail.com>
2022-11-30 15:54:45 +0530

    Finished task 8

---
+++ task-#8/README.md
@@ -0,0 +1,5 @@
+Followed the readme in the folder to complete the task.
+
+Google Fonts used :
+1. Rubik Glitch (header)
+2. Kaushan Script (buttons)
---
+++ task-#8/index.js
@@ -0,0 +1,35 @@
+const crash = new Audio('sounds/crash.mp3');
+function playCrash()
+    {
+        crash.play();
+    }
+const kick = new Audio('sounds/kick-bass.mp3');
+function playKick()
+    {
+        kick.play();
+    }
+const snare = new Audio('sounds/snare.mp3');
+function playSnare()
+    {
+        snare.play();
+    }
+const tom1 = new Audio('sounds/tom-1.mp3');
+function playTom1()
+    {
+        tom1.play();
+    }
+const tom2 = new Audio('sounds/tom-2.mp3');
+function playTom2()
+    {
+        tom2.play();
+    }
+const tom3 = new Audio('sounds/tom-3.mp3');
+function playTom3()
+    {
+        tom3.play();
+    }
+const tom4 = new Audio('sounds/tom-4.mp3');
+function playTom4()
+    {
+        tom4.play();
+    }
